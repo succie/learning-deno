8f6cfb386dd1de444c790217ed9d8900999ebc66
succie <kodai.suyama@gmail.com>
2020-05-16 14:22:06 +0900

    first commit

---
+++ index.test.ts
@@ -0,0 +1,18 @@
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import { add, sub, mul, div } from "./index.ts";
+
+Deno.test("add", () => {
+  assertEquals(add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), 55);
+});
+
+Deno.test("sub", () => {
+  assertEquals(sub(100, 50, 10), 40);
+});
+
+Deno.test("mul", () => {
+  assertEquals(mul(3, 4, 8), 96);
+});
+
+Deno.test("div", () => {
+  assertEquals(div(64, 2, 4), 8);
+});
